4e254beea63186a1054092eaf3eb6a3676f01c52
oumi0310 <k021g1382@g.neec.ac.jp>
2022-04-20 09:22:05 +0900

    01_03_kadai

--- main.cpp
+++ main.cpp
@@ -1,90 +1,99 @@
-#include<Windows.h>
-#include<d3d12.h>
-#include<dxgi1_6.h>
-#include<cassert>
-#include<vector>
-#include<string>
+#include <Windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+
+#include <vector>
+#include <string>
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 
-LRESULT WindowsProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	//メッセージで分岐
-	switch (msg)
-	{
-	case WM_DESTROY://ウィンドウが破棄された
-		PostQuitMessage(0);//OSに対して、アプリの終了を伝える
+#pragma region
+//ウィンドウプロシージャ
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
 		return 0;
 	}
 
-	return DefWindowProc(hwnd, msg, wparam, lparam);//標準の処理を行う
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-	const int windows_width = 1280;//横幅
-	const int windows_height = 720;//縦幅
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	OutputDebugStringA("Hello,DirectX!!\n");
 
-	WNDCLASSEX w{};//ウィンドウクラスの設定
+	const int window_width = 1280;
+	const int window_hegiht = 720;
+
+	//ウィンドウクラスの設定
+	WNDCLASSEX w{};
 	w.cbSize = sizeof(WNDCLASSEX);
-	w.lpfnWndProc = (WNDPROC)WindowsProc;//ウィンドウプロシージャを設定
-	w.lpszClassName = L"DirecyXGame";//ウィンドウクラス名
-	w.hInstance = GetModuleHandle(nullptr);//ウィンドウハンドル
-	w.hCursor = LoadCursor(NULL, IDC_ARROW);//カーソル指定
+	w.lpfnWndProc = (WNDPROC)WindowProc;
+	w.lpszClassName = L"DirectXGame";
+	w.hInstance = GetModuleHandle(nullptr);
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
 
-	//ウィンドウクラスをOSに登録
+	//ウィンドウクラスをOSに登録する
 	RegisterClassEx(&w);
-	//ウィンドウサイズ｛　X座標　Y座標　横幅　縦幅　｝
-	RECT wrc = { 0,0,windows_width,windows_height };
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);//自動でサイズ補正
+	//ウィンドウサイズ{ X座標　Y座標　横幅　縦幅　}
+	RECT wrc = { 0,0,window_width, window_hegiht };
+	//自動でサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
 	//ウィンドウオブジェクトの生成
-	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
-		L"DirectXGame",//タイトルバーの文字
-		WS_OVERLAPPEDWINDOW,//標準的なウィンドウスタイル
-		CW_USEDEFAULT,//表示X座標（OSに任せる）
-		CW_USEDEFAULT,//表示Y座標（OSに任せる）
-		wrc.right - wrc.left,//ウィンドウ横幅
-		wrc.bottom - wrc.top,//ウィンドウ縦幅
-		nullptr,//縦ウィンドウハンドル
-		nullptr,//メニューハンドル
-		w.hInstance,//呼び出しアプリケーション
-		nullptr);//オプション
+	HWND hwnd = CreateWindow(w.lpszClassName, //クラス名
+		L"DirectXGame",       //タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,  //標準的なウィンドウスタイル
+		CW_USEDEFAULT,        //表示X座標（OSに任せる）
+		CW_USEDEFAULT,        //表示Y座標（OSに任せる）
+		wrc.right - wrc.left, //ウィンドウ横幅
+		wrc.bottom - wrc.top, //ウィンドウ縦幅
+		nullptr,              //親ウィンドウハンドル
+		nullptr,              //メニューハンドル
+		w.hInstance,          //呼び出しアプリケーションハンドル
+		nullptr);             //オプション
 
-	//ウィンドウ表示
+	//ウィンドウを表示状態にする
 	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg{};//メッセージ
 
-	//DirectX初期化処理　ここから
 #ifdef _DEBUG
 	//デバックレイヤーをオンに
 	ID3D12Debug* debugController;
 	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
 		debugController->EnableDebugLayer();
 	}
-#endif
+#endif // DEBUG
 
+	//DirectX初期化処理　ここから
 	HRESULT result;
 	ID3D12Device* device = nullptr;
 	IDXGIFactory7* dxgiFactory = nullptr;
 	IDXGISwapChain4* swapChain = nullptr;
 	ID3D12CommandAllocator* cmdAllocator = nullptr;
-	ID3D12GraphicsCommandList* commandList = nullptr;
+	ID3D12GraphicsCommandList* commandlist = nullptr;
 	ID3D12CommandQueue* commandQueue = nullptr;
 	ID3D12DescriptorHeap* rtvHeap = nullptr;
+	//DirectX初期化処理　ここまで
 
 	//DXGIファクトリーの生成
 	result = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
 	assert(SUCCEEDED(result));
 
 	//アダプターの列挙用
-	std::vector<IDXGIAdapter4*>adapters;
+	std::vector < IDXGIAdapter4*>adapters;
 	//ここに特定の名前を持つアダプターオブジェクトが入る
 	IDXGIAdapter4* tmpAdapter = nullptr;
 
-	//パフォーマンスが高いものから順に、全てのアダプターを列挙する
+	//パフォーマンスが高いものから順に、すべてのアダプターを列挙する
 	for (UINT i = 0;
 		dxgiFactory->EnumAdapterByGpuPreference(i,
 			DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
@@ -94,16 +103,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		adapters.push_back(tmpAdapter);
 	}
 
-	//妥当なアダプタを追加する
-	for (size_t i = 0; i < adapters.size(); i++)
-	{
+	//妥当なアダプタを選別する
+	for (size_t i = 0; i < adapters.size(); i++) {
 		DXGI_ADAPTER_DESC3 adapterDesc;
-		//アダプターの情報を取得する
+		//アダプターの情報を収録する
 		adapters[i]->GetDesc3(&adapterDesc);
 
 		//ソフトウェアデバイスを回避
-		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE))
-		{
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
 			//デバイスを採用してループを抜ける
 			tmpAdapter = adapters[i];
 			break;
@@ -111,8 +118,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	}
 
 	//対応レベルの配列
-	D3D_FEATURE_LEVEL levels[] =
-	{
+	D3D_FEATURE_LEVEL levels[] = {
 		D3D_FEATURE_LEVEL_12_1,
 		D3D_FEATURE_LEVEL_12_0,
 		D3D_FEATURE_LEVEL_11_1,
@@ -122,7 +128,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	D3D_FEATURE_LEVEL featureLevel;
 
 	for (size_t i = 0; i < _countof(levels); i++) {
-		//採用したアダプターでデバイスを生成
 		result = D3D12CreateDevice(tmpAdapter, levels[i],
 			IID_PPV_ARGS(&device));
 		if (result == S_OK) {
@@ -132,7 +137,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		}
 	}
 
-	//コマンドアロケータを生成
+	//コマンドアロケータの生成
 	result = device->CreateCommandAllocator(
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
 		IID_PPV_ARGS(&cmdAllocator));
@@ -142,24 +147,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	result = device->CreateCommandList(0,
 		D3D12_COMMAND_LIST_TYPE_DIRECT,
 		cmdAllocator, nullptr,
-		IID_PPV_ARGS(&commandList));
+		IID_PPV_ARGS(&commandlist));
 	assert(SUCCEEDED(result));
 
 	//コマンドキューの設定
-	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
-	//コマンドキューの生成
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{}; //構造体の中身を0でクリアしている
+	//コマンドキューを生成
 	result = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
 	assert(SUCCEEDED(result));
 
 	//スワップチェーンの設定
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
-	swapChainDesc.Width = 1280;
+	swapChainDesc.Width = 1200;
 	swapChainDesc.Height = 720;
-	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;//色情報の書式
-	swapChainDesc.SampleDesc.Count = 1;//マルチサンプルしない
-	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;//バックバッファ用
-	swapChainDesc.BufferCount = 2;//バッファ数を2つに設定
-	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;//フリップ後は破棄
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
 	//スワップチェーンの生成
 	result = dxgiFactory->CreateSwapChainForHwnd(
@@ -169,8 +174,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	//デスクリプタヒープの設定
 	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
-	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;//レンダーターゲットビュー
-	rtvHeapDesc.NumDescriptors = swapChainDesc.BufferCount;//裏表の2つ
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; //レンダーターゲットビュー
+	rtvHeapDesc.NumDescriptors = swapChainDesc.BufferCount;
 
 	//デスクリプタヒープの生成
 	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
@@ -183,11 +188,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	for (size_t i = 0; i < backBuffers.size(); i++) {
 		//スワップチェーンからバッファを取得
 		swapChain->GetBuffer((UINT)i, IID_PPV_ARGS(&backBuffers[i]));
-		//デスクリプタヒープのハンドルを取得
+		//デスクリプタヒ－プのハンドルを取得
 		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
 		//裏か表かでアドレスがずれる
 		rtvHandle.ptr += i * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
-		//レンダーゲットビューの設定
+		//レンダーターゲットビューの設定
 		D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
 		//シェーダーの計算結果をSRGBに変換して書き込む
 		rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
@@ -202,28 +207,76 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	result = device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
 
-	//DirectX初期化処理　ここまで
+	// バックバッファの番号を取得(2つなので0番か1番)
+	UINT bbIndex = swapChain->GetCurrentBackBufferIndex();
+	// 1.リソースバリアで書き込み可能に変更
+	D3D12_RESOURCE_BARRIER barrierDesc{};
+	barrierDesc.Transition.pResource = backBuffers[bbIndex]; // バックバッファを指定
+	barrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT; // 表示状態から
+	barrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET; // 描画状態へ
+	commandlist->ResourceBarrier(1, &barrierDesc);
 
-	while (true)//ゲームループ
-	{
-		//メッセージがある？
-		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);//キー入力メッセージの処理
-			DispatchMessage(&msg);//プロシージャにメッセージを送る
+	// 2.描画先の変更
+// レンダーターゲットビューのハンドルを取得
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
+	rtvHandle.ptr += bbIndex * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
+	commandlist->OMSetRenderTargets(1, &rtvHandle, false, nullptr);
+
+	// 3.画面クリア R G B A
+	FLOAT clearColor[] = { 0.1f,0.25f, 0.5f,0.0f }; // 青っぽい色
+	commandlist->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+
+	// 4.描画コマンドここから
+
+	// 4.描画コマンドここまで
+
+	// 5.リソースバリアを戻す
+	barrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET; // 描画状態から
+	barrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT; // 表示状態へ
+	commandlist->ResourceBarrier(1, &barrierDesc);
+
+	// 命令のクローズ
+	result = commandlist->Close();
+	assert(SUCCEEDED(result));
+	// コマンドリストの実行
+	ID3D12CommandList* commandLists[] = { commandlist };
+	commandQueue->ExecuteCommandLists(1, commandLists);
+	// 画面に表示するバッファをフリップ(裏表の入替え)
+	result = swapChain->Present(1, 0);
+	assert(SUCCEEDED(result));
+
+	// コマンドの実行完了を待つ
+	commandQueue->Signal(fence, ++fenceVal);
+	if (fence->GetCompletedValue() != fenceVal) {
+		HANDLE event = CreateEvent(nullptr, false, false, nullptr);
+		fence->SetEventOnCompletion(fenceVal, event);
+		WaitForSingleObject(event, INFINITE);
+		CloseHandle(event);
+	}
+	// キューをクリア
+	result = cmdAllocator->Reset();
+	assert(SUCCEEDED(result));
+	// 再びコマンドリストを貯める準備
+	result = commandlist->Reset(cmdAllocator, nullptr);
+	assert(SUCCEEDED(result));
+#pragma endregion
+
+	//ゲームループ
+	while (true) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg); //キー入力メッセージの処理
+			DispatchMessage(&msg);  //プロシージャにメッセージを送る
 		}
 
 		//Xボタンで終了メッセージが来たらゲームループを抜ける
-		if (msg.message == WM_QUIT)
-		{
+		if (msg.message == WM_QUIT) {
 			break;
 		}
-		//DirectXフレーム処理　ここから
 
-		//DirectXフレーム処理　ここまで
+		//DirectX毎フレーム処理　ここから
+
+		//DirectX毎フレーム処理　ここまで
 	}
-	//ウィンドウクラスを登録解除
-	UnregisterClass(w.lpszClassName, w.hInstance);
-	
+
 	return 0;
 }
